d2027db82cb4a6c4094f19e8d777614a906f2216
14k <forever14k@gmail.com>
2017-10-17 17:03:32 +0300

    toSteamId64 and toSteamId32

--- source/@sisbf/steam/steam.utils.ts
+++ source/@sisbf/steam/steam.utils.ts
@@ -1,4 +1,16 @@
+import Big = require('big.js');
 import { SteamInventoryV1, SteamInventoryV2, SteamInventoryBaseAsset, SteamInventoryBaseDescription } from './inventory';
+
+
+export const STEAM_MAGIC_NUMBER = new Big('76561197960265728');
+
+export function toSteamId64(steamId32: string): string {
+    return STEAM_MAGIC_NUMBER.plus(steamId32).toString();
+}
+
+export function toSteamId32(steamId64: string): string {
+    return new Big(steamId64).minus(STEAM_MAGIC_NUMBER).toString();
+}
 
 
 export function toSteamInventoryV1(inventoryV2: SteamInventoryV2): SteamInventoryV1 {
