626e51e3f5b7ce3fe2247076568c476f342367cd
14k <forever14k@gmail.com>
2019-10-17 21:14:08 +0300

    add executor scheduler

---
+++ projects/sis/src/executor/executor-scheduler.spec.ts
@@ -0,0 +1,43 @@
+import { SchedulerLike } from 'rxjs';
+
+import { Executor, Job } from './executor';
+import { ExecutorScheduler } from './executor-scheduler';
+import { fakeAsync, tick } from '@angular/core/testing';
+
+
+describe('ExecutorScheduler', () => {
+
+    let executor: Executor;
+    let scheduler: SchedulerLike;
+    beforeEach(() => {
+        executor = {
+            execute: jasmine.createSpy('execute', (job: Job) => job.run()).and.callThrough(),
+        };
+        scheduler = new ExecutorScheduler(executor);
+    });
+
+    it('should schedule jobs in executor', () => {
+        const count = 14;
+        const jobs = Array(count).fill(null).map((_, index) => jasmine.createSpy(`job ${index}`));
+        for (const job of jobs) {
+            scheduler.schedule(job);
+        }
+        for (const job of jobs) {
+            expect(job).toHaveBeenCalledTimes(1);
+        }
+        expect(executor.execute).toHaveBeenCalledTimes(count);
+    });
+
+    it('should schedule jobs with delay', fakeAsync(() => {
+        const delay = 1000;
+        const halfDelay = delay / 2 ;
+        const job = jasmine.createSpy(`job`);
+        scheduler.schedule(job, delay);
+        expect(job).not.toHaveBeenCalled();
+        tick(halfDelay);
+        expect(job).not.toHaveBeenCalled();
+        tick(halfDelay);
+        expect(job).toHaveBeenCalledTimes(1);
+    }));
+
+});
--- projects/sis/src/executor/public-api.ts
+++ projects/sis/src/executor/public-api.ts
@@ -1,3 +1,4 @@
 export * from './executor';
+export * from './executor-scheduler';
 export * from './immediate-executor';
 export * from './steam-inventory-request-executor';
